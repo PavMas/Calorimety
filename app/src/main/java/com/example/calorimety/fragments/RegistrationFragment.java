68170a315bcb96a5f0c85210b3a543e4a432f5fb
pasha <pashamaslov12@gmail.com>
2022-05-14 03:19:37 +0300

    design v1

--- app/src/main/java/com/example/calorimety/fragments/RegistrationFragment.java
+++ app/src/main/java/com/example/calorimety/fragments/RegistrationFragment.java
@@ -1,4 +1,4 @@
-package com.example.calorimety;
+package com.example.calorimety.fragments;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.calorimety.R;
 
 /**
  * A simple {@link Fragment} subclass.
